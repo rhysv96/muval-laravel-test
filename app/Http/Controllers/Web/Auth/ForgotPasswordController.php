d41082bb5d0069d7f2938c3785bdd3b95e0630ce
rhysv96 <37141085+rhysv96@users.noreply.github.com>
2025-04-07 19:09:35 +1000

    Update ForgotPasswordController.php

--- app/Http/Controllers/Web/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Web/Auth/ForgotPasswordController.php
@@ -30,7 +30,7 @@ class ForgotPasswordController extends Controller
         $status = Password::sendResetLink($request->only('email'));
 
         return $status === Password::RESET_LINK_SENT
-            ? back()->with(['status' => __($status)])
+            ? back()->with(['state' => __($status)])
             : back()->withErrors(['email' => __($status)]);
     }
 
